c4d07638ef9e706aa5871c100e764c0c62087789
ln613 <ln613@hotmail.com>
2020-08-14 13:03:18 -0700

    host

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
-const url =
-  'http://localhost:9001/api?type={type}&cat={cat}';
+const isDev = process.env.NODE_ENV === 'development';
+const host = isDev ? 'http://localhost:9001' : '/.netlify/functions';
+const url = host + '/api?type={type}&cat={cat}';
 
 // export const loadCategories = () => ({
 //   path: 'cats',
